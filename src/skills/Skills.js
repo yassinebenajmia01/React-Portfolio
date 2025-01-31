c4bbfeebb4307ed3d0529adc366acfc1c447b8e0
yassinebenajmia01 <yacineajmiaskip7530@gmail.com>
2025-01-31 22:05:09 +0100

    31 janvier

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import Avatar from '../avatar/Avatar.js'
+import SkillsMenu from "./SkillsMenu.js"
 
-
-export default class About extends Component {
+export default class Skills extends Component {
   render() {
     return (
       <>
-        
+      <Avatar page="skills"/>
+      <SkillsMenu />
       </>
-    );
+    )
   }
 }
---
+++ src/skills/SkillsMenu.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SkillsMenu extends Component {
+  render() {
+    return (
+      <div>SkillsMenu</div>
+    )
+  }
+}
